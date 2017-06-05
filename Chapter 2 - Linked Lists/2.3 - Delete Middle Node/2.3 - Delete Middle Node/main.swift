1bba781a67d1e0db6afb084ff75e2e9280469707
Josh Henry <josh.d.henry@gmail.com>
2017-06-04 18:09:34 -0700

    Added question 2.3.

---
+++ Chapter 2 - Linked Lists/2.3 - Delete Middle Node/2.3 - Delete Middle Node/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  2.3 - Delete Middle Node
+//
+//  Created by Josh Henry on 6/4/17.
+//  Copyright © 2017 Big Smash Software. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
