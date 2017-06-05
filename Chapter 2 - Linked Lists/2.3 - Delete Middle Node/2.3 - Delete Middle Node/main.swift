a605ee69be89b4a4ce3134038fb897dc4520478e
Josh Henry <josh.d.henry@gmail.com>
2017-06-04 18:52:43 -0700

    Added question 3.2

--- Chapter 2 - Linked Lists/2.3 - Delete Middle Node/2.3 - Delete Middle Node/main.swift
+++ Chapter 2 - Linked Lists/2.3 - Delete Middle Node/2.3 - Delete Middle Node/main.swift
@@ -6,7 +6,50 @@
 //  Copyright © 2017 Big Smash Software. All rights reserved.
 //
 
+//Delete a node in the middle of a singly linked list (i.e. any node but the first and last node, not necessarily the exact middle) of a singly linked list, given only access to that node.
 import Foundation
 
-print("Hello, World!")
+class Node {
+    var data: Int
+    var next: Node?
+    
+    init(data: Int) {
+        self.data = data
+    }
+}
 
+
+func deleteNode(node: Node) {
+    node.data = (node.next?.data)!
+    node.next = node.next?.next
+}
+
+func printList(head: Node) {
+    var onNode: Node? = head
+    
+    while onNode != nil {
+        print(onNode!.data)
+        onNode = onNode!.next
+    }
+}
+
+var n1 = Node(data: 10)
+var n2 = Node(data: 20)
+n1.next = n2
+var n3 = Node(data: 30)
+n2.next = n3
+var n4 = Node(data: 40)
+n3.next = n4
+var n5 = Node(data: 50)
+n4.next = n5
+var n6 = Node(data: 60)
+n5.next = n6
+var n7 = Node(data: 70)
+n6.next = n7
+var n8 = Node(data: 80)
+n7.next = n8
+
+printList(head: n1)
+print("---")
+deleteNode(node: n5)
+printList(head: n1)
---
+++ Chapter 3 - Stacks and Queues/3.2 - Stack Min/3.2 - Stack Min/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  3.2 - Stack Min
+//
+//  Created by Josh Henry on 6/4/17.
+//  Copyright © 2017 Big Smash Software. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
