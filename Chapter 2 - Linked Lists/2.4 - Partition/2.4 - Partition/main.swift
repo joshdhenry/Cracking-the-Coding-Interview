4e23bf38c6f2ab972b978d4f64bb9a4a89384c36
Josh Henry <josh.d.henry@gmail.com>
2017-06-11 00:26:17 -0700

    Added question 2.4

---
+++ Chapter 2 - Linked Lists/2.4 - Partition/2.4 - Partition/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  2.4 - Partition
+//
+//  Created by Josh Henry on 6/10/17.
+//  Copyright © 2017 Big Smash Software. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
--- Chapter 4 - Trees and Graphs/4.2 - Minimal Tree/4.2 - Minimal Tree/main.swift
+++ Chapter 4 - Trees and Graphs/4.2 - Minimal Tree/4.2 - Minimal Tree/main.swift
@@ -6,7 +6,48 @@
 //  Copyright © 2017 Big Smash Software. All rights reserved.
 //
 
+//Given a sorted(increasing order) array with unique integer elements, write an algorithm to create a binary search tree with minimum height.
+
 import Foundation
 
-print("Hello, World!")
+class Node {
+    var data: Int
+    var left: Node?
+    var right: Node?
+    
+    init(data: Int) {
+        self.data = data
+    }
+}
 
+func createBST(inputArr: [Int], start: Int, end: Int) -> Node? {
+    if start > end {
+        return nil
+    }
+    
+    let mid = (start + end) / 2
+
+    let returnNode = Node(data: inputArr[mid])
+    returnNode.left = createBST(inputArr: inputArr, start: start, end: mid - 1)
+    returnNode.right = createBST(inputArr: inputArr, start: mid + 1, end: end)
+    
+    return returnNode
+}
+
+func printBST(root: Node) {
+    print(root.data)
+    print("---")
+
+    if let left = root.left {
+        print("LEFT")
+        printBST(root: left)
+    }
+    if let right = root.right {
+        print("RIGHT")
+        printBST(root: right)
+    }
+}
+
+let inputArr = [1, 2, 3, 4, 5, 6, 7]
+let root: Node? = createBST(inputArr: inputArr, start: 0, end: inputArr.count - 1)
+printBST(root: root!)
